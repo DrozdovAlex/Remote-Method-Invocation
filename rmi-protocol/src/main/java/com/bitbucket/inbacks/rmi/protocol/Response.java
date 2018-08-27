66cb3e7f5abc3fffa34d936af945b43bd59c11bf
Alex Drozdov <vip.alexd@gmail.com>
2018-08-27 16:29:46 +0700

    RMI-47 Create RequiredArgsConstructor in Request and Response
    Create RequiredArgsConstructor in Request and Response

--- rmi-protocol/src/main/java/com/bitbucket/inbacks/rmi/protocol/Response.java
+++ rmi-protocol/src/main/java/com/bitbucket/inbacks/rmi/protocol/Response.java
@@ -1,6 +1,6 @@
 package com.bitbucket.inbacks.rmi.protocol;
 
-import lombok.Getter;
+import lombok.*;
 
 import java.io.Serializable;
 
@@ -8,6 +8,7 @@ import java.io.Serializable;
  * This {@code Response} class represents a special message,
  * which is sent from the {@code Server} to the {@code Client}.
  */
+@RequiredArgsConstructor
 public class Response implements Serializable {
     /** Client id field */
     @Getter
@@ -20,19 +21,6 @@ public class Response implements Serializable {
     /** Error flag field */
     @Getter
     private final boolean error;
-
-    /**
-     * Constructs a new {@code Response} from {@code handle}.
-     *
-     * @param id - client id
-     * @param answer - answer
-     * @param error - error
-     */
-    public Response(Long id, Object answer, boolean error) {
-        this.id = id;
-        this.answer = answer;
-        this.error = error;
-    }
 
     /**
      * Returns {@code String} object with {@code id} and
