cc1355ead13a289ec79304a7b8cde3fe32e4c109
Alex Drozdov <vip.alexd@gmail.com>
2018-08-28 16:31:10 +0700

    RMI-55 Refactor Response and Request.
    Refactor Response and Request.

--- rmi-protocol/src/main/java/com/bitbucket/inbacks/rmi/protocol/Request.java
+++ rmi-protocol/src/main/java/com/bitbucket/inbacks/rmi/protocol/Request.java
@@ -3,46 +3,29 @@ package com.bitbucket.inbacks.rmi.protocol;
 import lombok.*;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * This {@code Request} class represents a special message,
  * which is sent from the {@code Client} to the {@code Server}.
  */
-@RequiredArgsConstructor
+@Data
+@Getter
 public class Request implements Serializable {
     /** Client id field */
-    @Getter
     private final Long id;
 
     /** Service name field */
-    @Getter
     private final String service;
 
     /** Method name field */
-    @Getter
     private final String method;
 
     /** Parameters field */
-    @Getter
     private final Object[] parameters;
 
     @Override
     public String toString() {
-        return id + " " + service+" "+ method + " " + getStringOfParameters(parameters);
-    }
-
-    /**
-     * Returns string of parameters.
-     *
-     * @param parameters specified parameters of the {@code method}
-     * @return string of parameters
-     */
-    private String getStringOfParameters(Object[] parameters) {
-        String result = "";
-
-        for (Object parameter : parameters) {
-            result = String.format(result + " %s", parameter);
-        }
-        return result;
+        return id + " " + service+" "+ method + " " + Arrays.toString(parameters);
     }
 }
