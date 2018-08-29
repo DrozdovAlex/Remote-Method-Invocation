545c7a1688d23818f612796caa8fda6b9186a6bc
alexkirnsu <alexkirnsu@gmail.com>
2018-08-29 13:13:53 +0700

    RMI-63 Add method to Service which throws exception

--- rmi-service/src/main/java/com/bitbucket/inbacks/rmi/service/Service.java
+++ rmi-service/src/main/java/com/bitbucket/inbacks/rmi/service/Service.java
@@ -9,8 +9,8 @@ public class Service {
     /**
      * Sends a thread to sleep for a certain time
      * @param millis - sleeping time
-     * @throws InterruptedException - if you catch this exception,
-     * it means that the thread has been interrupted
+     * @throws InterruptedException if you catch this exception,
+     *         it means that the thread has been interrupted
      */
     public void sleep(Long millis) throws InterruptedException {
         Thread.sleep(millis.longValue());
@@ -18,7 +18,7 @@ public class Service {
 
     /**
      * Returns the current date
-     * @return current date
+     * @return a current date
      */
     public Date getCurrentDate() {
         return new Date();
@@ -34,11 +34,25 @@ public class Service {
 
     /**
      * Returns the multiplication of numbers
-     * @param number1 - first multiplier
-     * @param number2 - second multiplier
+     * @param number1 a first multiplier
+     * @param number2 a second multiplier
      * @return multiplication of numbers
      */
     public Integer multiplication(Integer number1, Integer number2) {
         return number1 * number2;
+    }
+
+    /**
+     * Returns square root of signified parameter
+     *
+     * @param value a value
+     * @return square root of {@code value}
+     * @exception IllegalArgumentException if value is negative
+     */
+    public Double sqrt(Double value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("Negative argument");
+        }
+        return Math.sqrt(value);
     }
 }
